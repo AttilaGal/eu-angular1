cda6bc5321a6e59f949e5ea5d2280d87587e6aa6
Attila Gal <attila.gal@c4j.be>
2016-09-16 15:05:21 +0200

    prepared ch04-05

--- app/poke-list/poke-list.component.js
+++ app/poke-list/poke-list.component.js
@@ -1,30 +1,43 @@
 (function() {
-'use strict';
+  'use strict';
 
-    angular
-        .module('poke-list')
-        .component('pokeList', {
-            bindings: {
-                pokename: '@',
-                poketype: '@'
-            },
-            templateUrl: 'poke-list/poke-list.template.html',
-            controller: PokeListController,
-            controllerAs: 'pokeListCtrl'
-        });
+  angular
+    .module('poke-list')
+    .component('pokeList', {
+      bindings: {
+        pokename: '@',
+        poketype: '@'
+      },
+      templateUrl: 'poke-list/poke-list.template.html',
+      controller: PokeListController,
+      controllerAs: 'pokeListCtrl'
+    });
 
-    PokeListController.$inject = ['pokeService'];
-    function PokeListController(pokeService) {
-        var vm = this;
+  PokeListController.$inject = ['pokeService'];
+  function PokeListController(pokeService) {
+    var vm = this;
 
-        vm.isAwesome = function(pokename){
-            alert(pokename + ' is awesome!');
-        };
+    vm.evolutionInfo = function(pokename, id){
+      pokeService.getEvolutionById(id).then(
+        function success(evolution){
+          var message;
+          if(evolution){
+            message = pokename + ' evolves into ' + evolution + '!';
+          }else{
+            message = pokename + ' has no evolution.'
+          }
+          alert(message);
+        },
+        function error(){
+          alert('failed to retrieve pokemon evolution');
+        }
+      );
+    };
 
-        vm.classType = function(type){
-            return 'type-' + type;
-        };
+    vm.classType = function(type){
+      return 'type-' + type;
+    };
 
-        vm.pokedex = pokeService.getAll();
-    }
+    vm.pokedex = pokeService.getAll();
+  }
 })();
--- app/poke-service/poke-service.service.js
+++ app/poke-service/poke-service.service.js
@@ -5,16 +5,34 @@
 				.module('poke-service')
 				.factory('pokeService', PokeService);
 
-		PokeService.$inject = ['POKEDATA'];
-		function PokeService(POKEDATA) {
+		PokeService.$inject = ['POKEDATA', '$http', '$q'];
+		function PokeService(POKEDATA, $http, $q) {
 				var service = {
-						getAll: getAll
+						getAll: getAll,
+						getEvolutionById: getEvolutionById
 				};
 				return service;
 				
 				//function implementations
 				function getAll(){
 						return POKEDATA.pokedex;
+				}
+
+				function getEvolutionById(id){
+						return $q(function (resolve, reject) {
+								$http.get('http://pokeapi.co/api/v1/pokemon/' + id)
+										.then(
+												function success(response){
+														var evolution = null;
+														if(response.data.evolutions.length > 0){
+																evolution = response.data.evolutions[0].to;
+														}
+														resolve(evolution);
+												},
+												function error(response){
+														reject();
+												})
+						})
 				}
 		}
 
