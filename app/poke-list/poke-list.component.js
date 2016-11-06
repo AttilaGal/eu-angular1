82e52e1a1a9c27c2159f97c25a95570220772f78
Attila Gal <attila.gal@gmail.com>
2016-11-06 15:39:52 +0100

    reformatted al .js code to have 2 space indents

--- app/poke-list/poke-list.component.js
+++ app/poke-list/poke-list.component.js
@@ -1,30 +1,30 @@
-(function() {
-'use strict';
+(function () {
+  'use strict';
 
-    angular
-        .module('poke-list')
-        .component('pokeList', {
-            bindings: {
-                pokename: '@',
-                poketype: '@'
-            },
-            templateUrl: 'poke-list/poke-list.template.html',
-            controller: PokeListController,
-            controllerAs: 'pokeListCtrl'
-        });
+  angular
+    .module('poke-list')
+    .component('pokeList', {
+      bindings: {
+        pokename: '@',
+        poketype: '@'
+      },
+      templateUrl: 'poke-list/poke-list.template.html',
+      controller: PokeListController,
+      controllerAs: 'pokeListCtrl'
+    });
 
-    PokeListController.$inject = ['pokeService'];
-    function PokeListController(pokeService) {
-        var vm = this;
+  PokeListController.$inject = ['pokeService'];
+  function PokeListController(pokeService) {
+    var vm = this;
 
-        vm.isAwesome = function(pokename){
-            alert(pokename + ' is awesome!');
-        };
+    vm.isAwesome = function (pokename) {
+      alert(pokename + ' is awesome!');
+    };
 
-        vm.classType = function(type){
-            return 'type-' + type;
-        };
+    vm.classType = function (type) {
+      return 'type-' + type;
+    };
 
-        vm.pokedex = pokeService.getAll();
-    }
+    vm.pokedex = pokeService.getAll();
+  }
 })();
