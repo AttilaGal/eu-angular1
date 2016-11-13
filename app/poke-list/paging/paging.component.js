a1ab6702db6be3f8eb8ff786e220c1b5d32e220b
Attila Gal <attila.gal@c4j.be>
2016-11-13 17:48:50 +0100

    refactored components to using a store service

--- app/poke-list/paging/paging.component.js
+++ app/poke-list/paging/paging.component.js
@@ -5,7 +5,6 @@
     .module('poke-list')
     .component('paging', {
       bindings: {
-        callback: '&',
         totalItems: '@',
         perPage: '@'
       },
@@ -13,29 +12,34 @@
       controller: PagingController
     });
 
-  function PagingController() {
+  PagingController.$inject = ['pagingStore'];
+  function PagingController(pagingStore) {
     var vm = this;
-    vm.currentPage = 1;
+    vm.currentPage = pagingStore.getCurrentPage();
     vm.maxPage = Math.ceil(vm.totalItems / vm.perPage);
 
     vm.first = function () {
       vm.currentPage = 1;
-      vm.callback({page: vm.currentPage});
+      vm.updatePagingStore();
     };
 
     vm.previous = function () {
       vm.currentPage > 1 ? vm.currentPage-- : vm.currentPage;
-      vm.callback({page: vm.currentPage});
+      vm.updatePagingStore();
     };
 
     vm.next = function () {
       vm.currentPage < vm.maxPage ? vm.currentPage++ : null;
-      vm.callback({page: vm.currentPage});
+      vm.updatePagingStore();
     };
 
     vm.last = function () {
       vm.currentPage = vm.maxPage;
-      vm.callback({page: vm.currentPage});
+      vm.updatePagingStore();
+    };
+
+    vm.updatePagingStore = function(){
+      pagingStore.setCurrentPage(vm.currentPage);
     };
 
     vm.$onChanges = function (changesObj) {
