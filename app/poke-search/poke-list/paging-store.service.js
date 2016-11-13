6e0b7a8c51870155da7a28cf791a59231efda957
Attila Gal <attila.gal@gmail.com>
2016-11-13 23:10:12 +0100

    refactored to use paging store

---
+++ app/poke-search/poke-list/paging-store.service.js
@@ -0,0 +1,41 @@
+(function () {
+  "use strict";
+
+  angular
+    .module('poke-list')
+    .factory('pagingStore', PagingStoreController);
+
+  function PagingStoreController() {
+    var vm = this;
+    vm.listeners = {};
+    vm.store = {
+      currentPage: 1
+    };
+    vm.service = {
+      getCurrentPage: getCurrentPage,
+      setCurrentPage: setCurrentPage,
+      addListener: addListener,
+      removeListener: removeListener
+    };
+    return vm.service;
+
+
+    //function implementations
+    function getCurrentPage() {
+      return vm.store.currentPage;
+    }
+
+    function setCurrentPage(page){
+      vm.store.currentPage = page;
+      vm.listeners['page-change'] ? vm.listeners['page-change']() : null;
+    }
+
+    function addListener(name, callback){
+      vm.listeners[name] = callback;
+    }
+
+    function removeListener(name){
+      delete vm.listeners[name];
+    }
+  }
+})();
